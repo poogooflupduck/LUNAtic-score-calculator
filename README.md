2051921b829a11b5de2908c42d10312bb0d375ec
Karl Lee <poogooflupduck@gmail.com>
2021-11-10 04:15:39 +0000

    Add example addr

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # LUNAtic score calculator
 
+Discover your LUNAtic score, based on your participation in the Terra ecosystem.
+
+### Live Demo: https://lunatic-score-calculator.vercel.app/
+
+## Implementation
+
+This web app uses:
+
+- Data from [Flipside Crypto](https://flipsidecrypto.com/)
+- [Next.js](https://nextjs.org/) and [Tailwind CSS & UI](https://tailwindcss.com/) for the front-end
+
+### Scoring
+
+LUNAtic score is calculated by looking at the user's participation in staking, voting and depositing activities.
+The score is the sum of:
+
+Component | API endpoint
+Amount of LUNA staked | https://api.flipsidecrypto.com/api/v2/queries/663f6b3b-d6c8-4957-ba9c-ed5a74ab717d/data/latest
+Number of Anchor UST deposits | https://api.flipsidecrypto.com/api/v2/queries/93247971-5bcd-43c4-9542-095f8a12db1e/data/latest
+Number of Anchor ANC staking transactions | https://api.flipsidecrypto.com/api/v2/queries/d9120881-c198-4339-bf9c-682a47059d6f/data/latest
+Number of Terra governance votes cast | https://api.flipsidecrypto.com/api/v2/queries/f0cf0d93-d5a9-4f0f-be17-ba2e37f5a4e3/data/latest
+Number of Pylon Pool Deposits | https://api.flipsidecrypto.com/api/v2/queries/87724d52-076b-4e1a-98ef-8cd5db303ded/data/latest
+
+There is no upper limit to the score, nor is there any form of scaling factor. This allows the most invested Terra users to attain more ludicrous LUNAtic scores.
+
 ## Deploy your own
 
 Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example):
--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { ChevronRightIcon, StarIcon } from "@heroicons/react/solid";
 import { useState } from "react";
+import Link from "next/link";
 const Hero = (props) => {
   let [loading, setLoading] = useState(false);
   const router = useRouter();
@@ -57,18 +58,48 @@ const Hero = (props) => {
                 <div className="mt-4 sm:mt-0 sm:ml-3">
                   <button
                     type="submit"
-                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                    className="text-white block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-600 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none sm:px-10"
                   >
                     {loading && (
                       <svg
-                        className="z-2 animate-spin h-5 w-5 mr-3 text-white"
+                        class="animate-spin h-6 w-6 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
                         viewBox="0 0 24 24"
-                      ></svg>
+                      >
+                        <circle
+                          class="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          stroke-width="4"
+                        ></circle>
+                        <path
+                          class="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
                     )}
                     {!loading && <span>Go</span>}
                   </button>
                 </div>
               </form>
+              <div className="mt-6">
+                <div className="inline-flex items-center divide-x divide-gray-300">
+                  <div className="min-w-0 flex-1 py-1 text-sm text-gray-500 sm:py-3">
+                    <span className="font-medium text-gray-900">
+                      Or check out an example{" "}
+                      <Link href="/address/terra1nxtalu7p34jk4qrqutjf9y4qvte0hruxl9acg3">
+                        <a className="font-medium text-indigo-600">
+                          Terra LUNAtic
+                        </a>
+                      </Link>
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
